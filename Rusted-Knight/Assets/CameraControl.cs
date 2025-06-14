88ef902f29a9cd252c74e41af237b8e7a6af66bf
Shapeshifter24 <akash17844@gmail.com>
2025-06-14 15:52:15 -0400

    camera work

---
+++ Rusted-Knight/Assets/CameraControl.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+               
+    }
+}
