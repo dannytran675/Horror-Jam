9e43c059627b423e717c57c315b0316e476450ad
Danny Tran <chikinplayz@gmail.com>
2025-06-18 15:32:51 -0400

    worked on characterInfo class and Knight class

--- Rusted-Knight/Assets/CharacterInfo.cs
+++ Rusted-Knight/Assets/CharacterInfo.cs
@@ -2,20 +2,37 @@ using UnityEngine;
 
 public class CharacterInfo : MonoBehaviour
 {
-    public int hp, fam, crit;
+    public int hp, fam;
+    public float crit, rnd;
+    public bool guarded, usedMove;
 
-    public virtual void move1()
+    public virtual void move1(CharacterInfo character)
     {
 
     }
 
-    public virtual void move2()
+    public virtual void move2(CharacterInfo character)
     {
 
     }
 
-    public virtual void move3()
+    public virtual void move3(CharacterInfo character)
     {
 
+    }
+
+    public void setHP(int hp)
+    {
+        this.hp = hp;
+    }
+
+    public void setFam(int fam)
+    {
+        this.fam = fam;
+    }
+
+    public void addFam()
+    {
+        fam++;
     }
 }
