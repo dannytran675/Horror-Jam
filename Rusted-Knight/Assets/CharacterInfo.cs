66a26e518c03a7dd89f9219d1aece9db447f6534
Danny Tran <chikinplayz@gmail.com>
2025-06-18 14:12:54 -0400

    added characterinfo

---
+++ Rusted-Knight/Assets/CharacterInfo.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+    public int hp, fam, crit;
+
+    public virtual void move1()
+    {
+
+    }
+
+    public virtual void move2()
+    {
+
+    }
+
+    public virtual void move3()
+    {
+
+    }
+}
