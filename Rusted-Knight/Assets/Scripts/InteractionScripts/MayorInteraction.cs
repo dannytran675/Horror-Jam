6a5de9cd637d836647fbf20ce2884467e18eaadd
Danny Tran <chikinplayz@gmail.com>
2025-06-20 15:40:54 -0400

    Completed Dena interaction aswell as teleportation, starting work on DenaFlont as well as mayor interactions

---
+++ Rusted-Knight/Assets/Scripts/InteractionScripts/MayorInteraction.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class MayorInteraction : NPC
+{
+    public bool dialogueFinished;
+    public override void Interact()
+    {
+        if (GameManager.isFading || dialogueData == null || (PauseController.IsGamePaused && !isDialogueActive))
+        {
+            return;
+        }
+
+        if (isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
+    }
+
+    protected override void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            //Completes the line so it's fully on display
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if (++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            //If another line, type next line
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+}
